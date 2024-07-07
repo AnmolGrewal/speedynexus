13d4f3a4e227960ea9fefec687e1023da5456523
anmolgrewal <anmolsgrewal@gmail.com>
2024-07-07 14:19:21 -0700

    Adding tailwind colors to config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import colors from './tailwind-colors';
 
 const config: Config = {
   content: [
@@ -12,6 +13,10 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors,
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
       },
     },
   },
