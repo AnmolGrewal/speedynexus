fb158347d897ee77d6ffa69fecf4a3ac1b5ea15e
anmolgrewal <anmolsgrewal@gmail.com>
2024-07-07 18:49:59 -0700

    Adding Vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../app/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Analytics } from '@vercel/analytics/react';
 
 const darkTheme = createTheme({
   palette: {
@@ -12,6 +13,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={darkTheme}>
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
