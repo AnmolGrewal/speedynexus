16fc7772b8589deb194a504842e18ef7e9e80482
anmolgrewal <anmolsgrewal@gmail.com>
2024-07-07 18:20:21 -0700

    Adding title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Autocomplete, TextField, ThemeProvider, createTheme, Typography, Box } from '@mui/material';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider, DateCalendar } from '@mui/x-date-pickers';
+import { LocalizationProvider, DateCalendar, DatePicker } from '@mui/x-date-pickers';
 import NexusLocation, { locations } from '../data/Locations';
 import dayjs, { Dayjs } from 'dayjs';
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+
+dayjs.extend(isSameOrAfter);
+dayjs.extend(isSameOrBefore);
 
 const darkTheme = createTheme({
   palette: {
@@ -24,6 +29,8 @@ export default function Home() {
   const [selectedLocation, setSelectedLocation] = useState<NexusLocation | null>(null);
   const [timeSlots, setTimeSlots] = useState<TimeSlot[]>([]);
   const [selectedDate, setSelectedDate] = useState<Dayjs | null>(null);
+  const [fromDate, setFromDate] = useState<Dayjs | null>(null);
+  const [toDate, setToDate] = useState<Dayjs | null>(null);
 
   useEffect(() => {
     if (selectedLocation) {
@@ -55,29 +62,51 @@ export default function Home() {
     }
   };
 
+  const availableSlots = timeSlots.filter(slot => 
+    (!fromDate || dayjs(slot.startTimestamp).isSameOrAfter(fromDate, 'day')) &&
+    (!toDate || dayjs(slot.startTimestamp).isSameOrBefore(toDate, 'day'))
+  );
+
   return (
     <ThemeProvider theme={darkTheme}>
       <Box className="p-4" display="flex" flexDirection="column" alignItems="center">
+      <Typography variant="h3" align="center" gutterBottom>
+      Nexus Interview Availability
+    </Typography>
         <Autocomplete
           options={locations}
           getOptionLabel={(option) => option.name}
           renderInput={(params) => <TextField {...params} label="Select Location" />}
           onChange={(_, newValue) => setSelectedLocation(newValue)}
-          sx={{ width: 400, marginBottom: 2 }}
+          sx={{ width: 415, marginBottom: 2 }}
         />
-        {selectedLocation && (
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            {timeSlots.length > 0 ? (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <Box display="flex" justifyContent="center" mb={2}>
+            <DatePicker
+              label="From Date"
+              value={fromDate}
+              onChange={(newValue) => setFromDate(newValue)}
+              sx={{ width: 200, marginRight: 2 }}
+            />
+            <DatePicker
+              label="To Date"
+              value={toDate}
+              onChange={(newValue) => setToDate(newValue)}
+              sx={{ width: 200 }}
+            />
+          </Box>
+          {selectedLocation && (
+            availableSlots.length > 0 ? (
               <Box>
                 <Typography variant="h5" align="center" gutterBottom>
                   {selectedDate?.format('MMMM D, YYYY')}
                 </Typography>
                 <DateCalendar
                   sx={{ width: 400, height: 400 }}
-                  minDate={dayjs().startOf('day')}
-                  maxDate={dayjs().add(1, 'year').endOf('day')}
+                  minDate={fromDate || dayjs().startOf('day')}
+                  maxDate={toDate || dayjs().add(1, 'year').endOf('day')}
                   shouldDisableDate={(date) => {
-                    return !timeSlots.some(slot => dayjs(slot.startTimestamp).isSame(date, 'day'));
+                    return !availableSlots.some(slot => dayjs(slot.startTimestamp).isSame(date, 'day'));
                   }}
                   value={selectedDate}
                   onChange={(newDate) => setSelectedDate(newDate)}
@@ -87,7 +116,7 @@ export default function Home() {
                     Available Times
                   </Typography>
                   <ul style={{ listStyle: 'none', padding: 0, textAlign: 'center' }}>
-                    {timeSlots
+                    {availableSlots
                       .filter(slot => dayjs(slot.startTimestamp).isSame(selectedDate, 'day'))
                       .map(slot => (
                         <li key={slot.startTimestamp}>
@@ -101,9 +130,9 @@ export default function Home() {
               <Typography variant="h4" align="center">
                 No Dates Currently Available
               </Typography>
-            )}
-          </LocalizationProvider>
-        )}
+            )
+          )}
+        </LocalizationProvider>
       </Box>
     </ThemeProvider>
   );
