9368d62e03eefe8e701b0266d465ebc4341a1f3a
anmolgrewal <anmolsgrewal@gmail.com>
2024-07-07 18:59:52 -0700

    Updating locations alphabetically

--- src/data/Locations.tsx
+++ src/data/Locations.tsx
@@ -1,125 +1,125 @@
 interface NexusLocation {
+  address: string;
   id: number;
   name: string;
-  address: string;
   region: string;
 }
 
 export const locations: NexusLocation[] = [
   {
-    id: 5500,
-    name: "Calais Enrollment Center",
-    address: "3 Customs Street, Calais, MAINE 04619",
-    region: "MAINE"
-  },
-  {
-    id: 5101,
-    name: "Houlton POE/Woodstock",
-    address: "27 Customs Loop, Houlton, MAINE 04730",
-    region: "MAINE"
-  },
-  {
-    id: 16511,
-    name: "Detroit Enrollment Center NEXUS",
-    address: "2810 W. Fort Street Suite 124, Detroit, MICHIGAN 48216",
-    region: "MICHIGAN"
-  },
-  {
-    id: 16656,
-    name: "DETROIT NEXUS/FAST",
-    address: "2810 W. FORT STREET SUITE 124, DETROIT, MICHIGAN 48216",
-    region: "MICHIGAN"
-  },
-  {
-    id: 5024,
-    name: "Port Huron Enrollment Center",
-    address: "2321 Pine Grove Avenue, Port Huron, MICHIGAN 48060",
-    region: "MICHIGAN"
-  },
-  {
-    id: 5080,
-    name: "Sault Ste Marie Enrollment Center",
-    address: "900 International Bridge Plaza, Sault Ste. Marie, MICHIGAN 49783",
-    region: "MICHIGAN"
-  },
-  {
-    id: 5160,
-    name: "International Falls NEXUS/FAST Enrollment Center",
-    address: "3214 2nd Ave E, International Falls, MINNESOTA 56649",
-    region: "MINNESOTA"
-  },
-  {
-    id: 5060,
-    name: "Warroad Enrollment Center",
-    address: "41059 Warroad Enrollment Center, Warroad, MINNESOTA 56763",
-    region: "MINNESOTA"
-  },
-  {
-    id: 5120,
-    name: "Sweetgrass NEXUS and FAST Enrollment Center",
-    address: "Nexus Enrollment Center, Sweetgrass, MONTANA 59484",
-    region: "MONTANA"
-  },
-  {
-    id: 5021,
-    name: "Champlain NEXUS and FAST",
-    address: "237 West Service Road, Champlain, NEW YORK 12919",
-    region: "NEW YORK"
-  },
-  {
-    id: 5161,
-    name: "Niagara Falls Enrollment Center",
-    address: "2250 Whirlpool St, Niagara Falls, NEW YORK 14305",
-    region: "NEW YORK"
-  },
-  {
-    id: 16712,
-    name: "Niagara Falls Enrollment Center NEXUS",
-    address: "2250 Whirlpool St, Niagara Falls, NEW YORK 14305",
-    region: "NEW YORK"
-  },
-  {
-    id: 16546,
-    name: "Ogdensburg Enrollment Center",
-    address: "104 Bridge Approach Road, Ogdensburg, NEW YORK 13669",
-    region: "NEW YORK"
-  },
-  {
-    id: 5100,
-    name: "Pembina Enrollment Center",
-    address: "10980 Interstate 29, Pembina, NORTH DAKOTA 58271",
-    region: "NORTH DAKOTA"
-  },
-  {
-    id: 5223,
-    name: "Derby Line Enrollment Center",
-    address: "107 I-91 South, Derby Line, VERMONT 05830",
-    region: "VERMONT"
-  },
-  {
+    address: '8115 Birch Bay Square St., BLAINE, WASHINGTON 98230',
     id: 5020,
-    name: "Blaine NEXUS And FAST Enrollment Center",
-    address: "8115 Birch Bay Square St., BLAINE, WASHINGTON 98230",
-    region: "WASHINGTON"
+    name: 'Blaine NEXUS And FAST Enrollment Center',
+    region: 'WASHINGTON',
   },
   {
-    id: 16764,
-    name: "SEAFO-Blaine, WA Nexus Enrollment Blitz 2024",
-    address: "8115 Birch Bay Square St #104, Blaine, WASHINGTON 98230",
-    region: "WASHINGTON"
-  },
-  {
+    address: '10 Central Ave, Fort Erie, ONTARIO L2A3S6',
     id: 5022,
-    name: "Buffalo-Ft. Erie Enrollment Center",
-    address: "10 Central Ave, Fort Erie, ONTARIO L2A3S6",
-    region: "Ontario"
+    name: 'Buffalo-Ft. Erie Enrollment Center',
+    region: 'Ontario',
   },
   {
+    address: '3 Customs Street, Calais, MAINE 04619',
+    id: 5500,
+    name: 'Calais Enrollment Center',
+    region: 'MAINE',
+  },
+  {
+    address: '237 West Service Road, Champlain, NEW YORK 12919',
+    id: 5021,
+    name: 'Champlain NEXUS and FAST',
+    region: 'NEW YORK',
+  },
+  {
+    address: '107 I-91 South, Derby Line, VERMONT 05830',
+    id: 5223,
+    name: 'Derby Line Enrollment Center',
+    region: 'VERMONT',
+  },
+  {
+    address: '2810 W. Fort Street Suite 124, Detroit, MICHIGAN 48216',
+    id: 16511,
+    name: 'Detroit Enrollment Center NEXUS',
+    region: 'MICHIGAN',
+  },
+  {
+    address: '2810 W. FORT STREET SUITE 124, DETROIT, MICHIGAN 48216',
+    id: 16656,
+    name: 'DETROIT NEXUS/FAST',
+    region: 'MICHIGAN',
+  },
+  {
+    address: '27 Customs Loop, Houlton, MAINE 04730',
+    id: 5101,
+    name: 'Houlton POE/Woodstock',
+    region: 'MAINE',
+  },
+  {
+    address: '3214 2nd Ave E, International Falls, MINNESOTA 56649',
+    id: 5160,
+    name: 'International Falls NEXUS/FAST Enrollment Center',
+    region: 'MINNESOTA',
+  },
+  {
+    address: '860 Highway 137, Lansdowne, ONTARIO K0E1L0',
     id: 5520,
-    name: "Lansdowne (Thousand Islands Bridge)",
-    address: "860 Highway 137, Lansdowne, ONTARIO K0E1L0",
-    region: "Ontario"
-  }
+    name: 'Lansdowne (Thousand Islands Bridge)',
+    region: 'Ontario',
+  },
+  {
+    address: '2250 Whirlpool St, Niagara Falls, NEW YORK 14305',
+    id: 5161,
+    name: 'Niagara Falls Enrollment Center',
+    region: 'NEW YORK',
+  },
+  {
+    address: '2250 Whirlpool St, Niagara Falls, NEW YORK 14305',
+    id: 16712,
+    name: 'Niagara Falls Enrollment Center NEXUS',
+    region: 'NEW YORK',
+  },
+  {
+    address: '104 Bridge Approach Road, Ogdensburg, NEW YORK 13669',
+    id: 16546,
+    name: 'Ogdensburg Enrollment Center',
+    region: 'NEW YORK',
+  },
+  {
+    address: '10980 Interstate 29, Pembina, NORTH DAKOTA 58271',
+    id: 5100,
+    name: 'Pembina Enrollment Center',
+    region: 'NORTH DAKOTA',
+  },
+  {
+    address: '2321 Pine Grove Avenue, Port Huron, MICHIGAN 48060',
+    id: 5024,
+    name: 'Port Huron Enrollment Center',
+    region: 'MICHIGAN',
+  },
+  {
+    address: '900 International Bridge Plaza, Sault Ste. Marie, MICHIGAN 49783',
+    id: 5080,
+    name: 'Sault Ste Marie Enrollment Center',
+    region: 'MICHIGAN',
+  },
+  {
+    address: '8115 Birch Bay Square St #104, Blaine, WASHINGTON 98230',
+    id: 16764,
+    name: 'SEAFO-Blaine, WA Nexus Enrollment Blitz 2024',
+    region: 'WASHINGTON',
+  },
+  {
+    address: 'Nexus Enrollment Center, Sweetgrass, MONTANA 59484',
+    id: 5120,
+    name: 'Sweetgrass NEXUS and FAST Enrollment Center',
+    region: 'MONTANA',
+  },
+  {
+    address: '41059 Warroad Enrollment Center, Warroad, MINNESOTA 56763',
+    id: 5060,
+    name: 'Warroad Enrollment Center',
+    region: 'MINNESOTA',
+  },
 ];
 
 export default NexusLocation;
